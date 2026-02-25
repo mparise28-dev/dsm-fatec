a4f41bcdaf76a0f9b8820dfdb0fd86807fb0816c
mparise28-dev <marcelloparise28@gmail.com>
2026-02-25 16:17:21 -0300

    Add files via upload

---
+++ ex02.js
@@ -0,0 +1,9 @@
+nro1 = "8"//prompt("Entre com um número")
+nro2 = "15"//prompt("Entre com segundo número")
+nro1 = parseInt(nro1)
+nro2 = parseInt(nro2)
+
+while (nro1 <= nro2) {
+    console.log(nro1)
+    nro1 = nro1 + 1
+}
